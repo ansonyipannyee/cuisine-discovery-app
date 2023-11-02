f712b1bcc4a30414f836213ea13c4c86aca3a194
Anson Yip Ann Yee <ansonyipannyee@outlook.com>
2023-11-02 21:19:22 +1100

    mvp trial app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [cuisines, setCuisines] = useState([]);
+  const [selectedCuisine, setSelectedCuisine] = useState(null);
+  const [dishes, setDishes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCuisines(data.meals);
+        setLoading(false);
+      })
+      .catch((error) => console.error("Error fetching cuisines:", error));
+  }, []);
+
+  useEffect(() => {
+    if (selectedCuisine) {
+      fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedCuisine.strCategory}`)
+        .then((response) => response.json())
+        .then((data) => setDishes(data.meals))
+        .catch((error) => console.error("Error fetching dishes:", error));
+    }
+  }, [selectedCuisine]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cuisine Discovery</h1>
+      {loading ? (
+        <p>Loading cuisines...</p>
+      ) : (
+        <div className="cuisine-list">
+          <h2>Cuisines</h2>
+          <ul>
+            {cuisines.map((cuisine) => (
+              <li key={cuisine.strCategory}>
+                <button onClick={() => setSelectedCuisine(cuisine)}>
+                  {cuisine.strCategory}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {selectedCuisine && (
+        <div className="dishes-list">
+          <h2>Dishes from {selectedCuisine.strCategory}</h2>
+          <ul>
+            {dishes.map((dish) => (
+              <li key={dish.idMeal}>{dish.strMeal}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
