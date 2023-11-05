1426f240bc8af6fa82d539602d076aa255a44220
Anson Yip Ann Yee <ansonyipannyee@outlook.com>
2023-11-05 16:24:46 +1100

    sidebar onclick

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-const Sidebar = ({ cuisines, onCuisineClick }) => {
+function Sidebar({ cuisines, onCuisineClick }) {
     return (
         <div className="sidebar">
+            <h2>Cuisines</h2>
             <ul>
                 {cuisines.map((cuisine) => (
-                    <li key={cuisine.strArea}>
-                        <button onClick={() => onCuisineClick(cuisine)}> {cuisine.strArea}</button>
+                    <li key={cuisine.strCategory}>
+                        <button onClick={() => onCuisineClick(cuisine)}>
+                            {cuisine.strCategory}
+                        </button>
                     </li>
                 ))}
             </ul>
