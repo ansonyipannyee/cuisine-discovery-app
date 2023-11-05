668a887fe8ff2de13ec3f8b669c051927218bc0e
Anson Yip Ann Yee <ansonyipannyee@outlook.com>
2023-11-06 02:07:30 +1100

    fingers broken

--- src/DishItem.js
+++ src/DishItem.js
@@ -1,12 +1,31 @@
 import React from "react";
 
-const DishItem = ({dish}) => {
-    return (
-        <div className="dish">
-            <img src={dish.strMealThumb} alt={dish.strMeal} />
-            <p>{dish.strMeal}</p>
+const DishItem = ({ dish, onRecipeClick }) => {
+  return (
+    <div className="dish">
+      <img src={dish.strMealThumb} alt={dish.strMeal} />
+      <p>{dish.strMeal}</p>
+      {dish.strInstructions && (
+        <div className="recipe">
+          <h2>Recipe</h2>
+          <p>{dish.strInstructions}</p>
+          {dish.strSource && (
+            <div>
+              <img src={dish.strMealThumb} />
+              <a
+                href={dish.strSource}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Watch video
+              </a>
+            </div>
+          )}
         </div>
-    );
+      )}
+      <button onClick={onRecipeClick}>View Recipe</button>
+    </div>
+  );
 };
 
 export default DishItem;
