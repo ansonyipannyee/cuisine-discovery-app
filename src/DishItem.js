e8531750962546f2e116d110fdfc51fed1eb9667
Anson Yip Ann Yee <ansonyipannyee@outlook.com>
2023-11-06 10:02:56 +1100

    css for column layout

--- src/DishItem.js
+++ src/DishItem.js
@@ -5,24 +5,6 @@ const DishItem = ({ dish }) => {
     <div className="dish">
       <img src={dish.strMealThumb} alt={dish.strMeal} />
       <p>{dish.strMeal}</p>
-      {/* {dish.strInstructions && (
-        <div className="recipe">
-          <h2>Recipe</h2>
-          <p>{dish.strInstructions}</p>
-          {dish.strSource && (
-            <div>
-              <img src={dish.strMealThumb} />
-              <a
-                href={dish.strSource}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Watch video
-              </a>
-            </div>
-          )}
-        </div>
-      )} */}
     </div>
   );
 };
